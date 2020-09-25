c34f2b2a12c8b7f58df76d7161f40d817d44bb7a
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-09-25 15:06:02 +0530

    Added UC2 to calculate daily employee wage

--- EmpWage_Builder.java
+++ EmpWage_Builder.java
@@ -1,12 +1,16 @@
 public class EmpWage_Builder{
 	//Constants
 	public static final int IS_FULL_TIME = 1;
+	public static final int EMP_RATE_PER_HOUR = 20;
 	public static void main(String[] args){
+		//Variables
+		int empHrs = 0;
+		int empWage = 0; //int empWage; would also work
 		//Computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if(empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+			empHrs = 8;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 }
